f84decbd1ecdeade648d290ce4edc55f8ae0c76c
Tim Koelkebeck <tim@mytype.com>
2011-04-29 14:07:06 -0400

    initial commit with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libertyhive::Application.config.secret_token = '7601ec69d8dbb4e90746637fbb7dbb04869faf10d5e144c426320befe5faffb39c751a43ebab603d311f08ef5b600387d21dd36830bcc4c0985b23a38e55af9e'
