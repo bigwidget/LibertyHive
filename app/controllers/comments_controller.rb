b46aaa9e82e45216efa60b35f0f13e01a7045f1f
Tim Koelkebeck <tim@mytype.com>
2011-05-17 18:16:16 -0400

    controllers working

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+  def show
+    @comment = Comment.find(params[:id])
+    @title = "should be first ~100 characters in post"
+  end
+
+  def create
+    @comment = current_user.comments.build(params[:comment])
+    if @comment.save
+      flash[:success] = "Comment posted!"
+      @comment.send_notification
+      redirect_to (@comment.parent ? @comment.parent : @comment.link)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @comment.destroy
+  end
+
+
+end
---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,33 @@
+class VotesController < ApplicationController
+
+  before_filter :authenticate_user!
+  
+  def create
+    @votable = Vote.new(params[:vote]).votable
+    current_user.vote_for!(@votable)
+  
+    if params[:vote][:votable_type] == "Link"
+      redirect_to links_path  #stay on links page
+    else
+      redirect_to Link.find(@votable.link_id)   #stay on link discussion page
+    end
+  end
+  
+  def destroy
+    #currently no way to destroy a vote
+  end
+  
+  private
+  
+    # do I need this method?
+    def find_votable
+      params.each do |name, value|
+        if name =~ /(.+)_id$/
+          return $1.classify.constantize.find(value)
+        end
+      end  
+      return nil
+    end
+
+
+end
