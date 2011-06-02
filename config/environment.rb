18d85bced008e06f740656e69105c48e465e2c14
Tim Koelkebeck <tim@mytype.com>
2011-06-02 00:11:45 -0400

    moved require uri_validator to top of environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
 PER_PAGE = 20
 
+require 'uri_validator'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Libertyhive::Application.initialize!
-
-require 'uri_validator'
